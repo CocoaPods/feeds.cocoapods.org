4b000b109fdbf26c9f296471ede477548786c4fc
Fabio Pelosin <fabiopelosin@gmail.com>
2012-04-18 22:23:42 +0200

    [Tests] splitted rss tests in smaller units

--- test/rss_test.rb
+++ test/rss_test.rb
@@ -15,54 +15,103 @@ class RSSTest < Test::Unit::TestCase
 
       @pods = @sets.map { |set| Pod::Command::Presenter::CocoaPod.new(set) }
 
+      # SSZipArchive has been created most recently, so comes first
+      # JSONKit has been created earlier, so comes last
       @creation_dates = {
         'JSONKit'      => Time.now - 60,
         'SSZipArchive' => Time.now - 30,
       }
 
-      @rss = CocoapodFeed::Rss.new(@pods, @creation_dates)
-      @root = REXML::Document.new(@rss.feed).root
+      @rss   = CocoapodFeed::Rss.new(@pods, @creation_dates)
+      @root  = REXML::Document.new(@rss.feed).root
+      item1  = @root.elements['channel/item[1]']
+      item2  = @root.elements['channel/item[2]']
+      @desc1 = REXML::Document.new("<root>#{item1.elements['description'].text}</root>").root
+      @desc2 = REXML::Document.new("<root>#{item2.elements['description'].text}</root>").root
     end
   end
 
-  # TODO this needs to be refactored to test smaller units
-  #
-  # E.g.
-  #
-  # def test_it_uses_the_pod_titles_as_item_title
-  #   assert_equal 'SSZipArchive', @root.elements['channel/item[1]/title'].text
-  #   assert_equal 'JSONKit',      @root.elements['channel/item[2]/title'].text
-  # end
+  # Check RSS Items
 
-  def test_it_generates_a_rss_document
-    root = @root
-    assert_equal 'CocoaPods', root.elements['channel/title'].text
-
-    # SSZipArchive has been created most recently, so comes first
-    item = root.elements['channel/item[1]']
-    assert_equal 'SSZipArchive', item.elements['title'].text
-    homepage = 'https://github.com/samsoffes/ssziparchive'
-    assert_equal homepage, item.elements['link'].text
-    description = REXML::Document.new("<root>#{item.elements['description'].text}</root>").root
-    assert_equal 'SSZipArchive is a simple utility class for zipping and unzipping files on iOS and Mac.', description.elements['p[1]'].text
-    assert_equal "<p>[ by Sam Soffes | available at: <a href='#{homepage}.git'>github.com</a> ]</p>", description.elements['p[2]'].to_s
-    assert_equal "Latest version: 0.1.2", description.elements['ul/li[1]'].text
-    assert_equal "Platform: iOS - OS X", description.elements['ul/li[2]'].text
-    assert_match /Watchers: \d+/, description.elements['ul/li[3]'].text
-    assert_match /Forks: \d+/, description.elements['ul/li[4]'].text
-
-    # JSONKit has been created earlier, so comes last
-    item = root.elements['channel/item[2]']
-    assert_equal 'JSONKit', item.elements['title'].text
-    homepage = 'https://github.com/johnezang/JSONKit'
-    assert_equal homepage, item.elements['link'].text
-    description = REXML::Document.new("<root>#{item.elements['description'].text}</root>").root
-    assert_equal 'A Very High Performance Objective-C JSON Library.', description.elements['p[1]'].text
-    assert_equal "<p>[ by John Engelhart | available at: <a href='#{homepage}.git'>github.com</a> ]</p>", description.elements['p[2]'].to_s
-    assert_equal "Latest version: 1.4", description.elements['ul/li[1]'].text
-    assert_equal "Platform: iOS - OS X", description.elements['ul/li[2]'].text
-    assert_equal "License: BSD / Apache License, Version 2.0", description.elements['ul/li[3]'].text
-    assert_match /Watchers: \d+/, description.elements['ul/li[4]'].text
-    assert_match /Forks: \d+/, description.elements['ul/li[5]'].text
+  def check_values(item_node)
+    value1 = @root.elements["channel/item[1]/#{item_node}"].text
+    value2 = @root.elements["channel/item[2]/#{item_node}"].text
+    [value1, value2]
   end
+
+  def test_it_uses_the_pod_titles_as_item_title
+    value1, value2 = check_values('title')
+    assert_equal value1, 'SSZipArchive'
+    assert_equal value2, 'JSONKit'
+  end
+
+  def test_it_uses_the_pod_homepages_as_item_link
+    value1, value2 = check_values('link')
+    assert_equal value1, 'https://github.com/samsoffes/ssziparchive'
+    assert_equal value2, 'https://github.com/johnezang/JSONKit'
+  end
+
+  # Check Items Descriptions
+
+  def check_description_values(desc_node_1, desc_node_2 = nil)
+    desc_node_2 = desc_node_1 unless desc_node_2
+    value1 = @desc1.elements[desc_node_1].text
+    value2 = @desc2.elements[desc_node_2].text
+    [value1, value2]
+  end
+
+  def test_it_shows_the_pod_description
+    value1, value2 = check_description_values('p[1]')
+    assert_equal value1, 'SSZipArchive is a simple utility class for zipping and unzipping files on iOS and Mac.'
+    assert_equal value2, 'A Very High Performance Objective-C JSON Library.'
+  end
+
+  def test_it_shows_the_pod_author_and_the_repo_link
+    value1 = @desc1.elements['p[2]'].to_s
+    value2 = @desc2.elements['p[2]'].to_s
+    assert_equal value1.to_s, "<p>[ by Sam Soffes | available at: <a href='https://github.com/samsoffes/ssziparchive.git'>github.com</a> ]</p>"
+    assert_equal value2.to_s, "<p>[ by John Engelhart | available at: <a href='https://github.com/johnezang/JSONKit.git'>github.com</a> ]</p>"
+
+  end
+
+  def test_it_shows_the_pod_description
+    value1, value2 = check_description_values('p[1]')
+    assert_equal value1, 'SSZipArchive is a simple utility class for zipping and unzipping files on iOS and Mac.'
+    assert_equal value2, 'A Very High Performance Objective-C JSON Library.'
+  end
+
+  def test_it_shows_the_pod_version
+    value1, value2 = check_description_values('ul/li[1]')
+    assert_equal value1, "Latest version: 0.1.2"
+    assert_equal value2, "Latest version: 1.4"
+  end
+
+  def test_it_shows_the_pod_platform
+    value1, value2 = check_description_values('ul/li[2]')
+    assert_equal value1, "Platform: iOS - OS X"
+    assert_equal value2, "Platform: iOS - OS X"
+  end
+
+  def test_it_shows_the_pod_license
+    value1, value2 = check_description_values('ul/li[3]')
+    # SSZipArchive doesn't have a license
+    assert_equal value2, "License: BSD / Apache License, Version 2.0"
+  end
+
+  def test_it_shows_the_pod_github_watchers
+    value1, value2 = check_description_values('ul/li[3]', 'ul/li[4]')
+    assert_match value1, /Watchers: \d+/
+    assert_match value2, /Watchers: \d+/
+  end
+
+   def test_it_shows_the_pod_github_forks
+    value1, value2 = check_description_values('ul/li[4]', 'ul/li[5]')
+    assert_match value1, /Forks: \d+/
+    assert_match value2, /Forks: \d+/
+  end
+
+  def test_it_generates_the_CocoaPods_channel
+    assert_equal 'CocoaPods', @root.elements['channel/title'].text
+  end
+
 end

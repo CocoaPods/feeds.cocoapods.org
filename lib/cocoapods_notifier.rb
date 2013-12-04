2aacb9cab04582973d3884474af74e423647446c
Eloy Durán <eloy.de.enige@gmail.com>
2013-12-04 18:53:43 +0100

    Silence I18n deprecation warning.

--- lib/cocoapods_notifier.rb
+++ lib/cocoapods_notifier.rb
@@ -3,6 +3,10 @@ module CocoaPodsNotifier
   require 'cocoapods-core'
   Pod = ::Pod
 
+  # Only to silence the deprecation warning.
+  require 'i18n'
+  I18n.enforce_available_locales = false
+
   require 'cocoapods_notifier/repo'
   require 'cocoapods_notifier/rss'
   require 'cocoapods_notifier/twitter_notifier'

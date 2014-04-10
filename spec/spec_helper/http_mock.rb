a2e517a3a7367d8bf36a83bebc964dd384837e8b
Fabio Pelosin <fabiopelosin@gmail.com>
2014-04-10 18:29:15 +0200

    [Specs] fix http_mock for Ruby 2.1

--- spec/spec_helper/http_mock.rb
+++ spec/spec_helper/http_mock.rb
@@ -8,6 +8,10 @@ module Net
   class HTTP
     class TryingToMakeHTTPConnectionException < StandardError; end
 
+    def begin_transport(argument)
+      raise TryingToMakeHTTPConnectionException, "Please mock your HTTP calls so you don't do any HTTP requests."
+    end
+
     def connect
       raise TryingToMakeHTTPConnectionException, "Please mock your HTTP calls so you don't do any HTTP requests."
     end
